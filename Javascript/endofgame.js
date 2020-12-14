39251fc84f460873d624666cbd550364180c83d0
eubank87 <eubank.ashley@gmail.com>
2020-12-14 08:30:37 -0800

    created conditional so empty input field cannot be saved using event listener for keyup.

--- Javascript/endofgame.js
+++ Javascript/endofgame.js
@@ -6,5 +6,16 @@ var finalScore = document.getElementById("finalScore");
 // variables used to save newest score achieved to high scores list
 var newestScore = localStorage.getItem("newestScore");
 var highScores = JSON.parse(localStorage.getItem("highScores")) || [];
-console.log("high scores:", highScores);
+// console.log("high scores:", highScores);
 var maxHighScores = 5;
+
+// displaying final score onto the screen
+finalScore.innerText = newestScore;
+
+// created event listener for input field to listen for "key up"
+playerName.addEventListener("keyup", () =>{
+    // console.log("player name:", playerName.value);
+    // created conditional so save score button is disabled until input field is filled out
+    saveHighScoreBtn.disabled = !playerName.nodeValue; 
+});
+
